b1f98e6b33cab9d846169d39d7c91033e20bc2e0
Andrii Kurdiumov <kant2002@gmail.com>
2024-03-21 18:15:00 +0500

    Звіт по усім змінним оточення

--- DiiaTools/Програма.cs
+++ DiiaTools/Програма.cs
@@ -3,6 +3,7 @@ using Система;
 using Система.ВВ;
 
 var папкаІзДією = args[0];
+var усіЗмінні = new Dictionary<string, ЗміннаОточення>();
 foreach (var папкаМодуля in Directory.EnumerateDirectories(папкаІзДією))
 {
     var файлОписа = Path.Combine(папкаМодуля, "package.json");
@@ -47,7 +48,36 @@ foreach (var папкаМодуля in Directory.EnumerateDirectories(папка
         }
     }
 
-    Console.WriteLine($"# Модуль {Path.GetFileName(папкаМодуля)}");
+    var назваМодуля = Path.GetFileName(папкаМодуля);
+    //НадрукуватиЗмінні(назваМодуля, змінні);
+    ОбєднатиЗмінні(назваМодуля, змінні);
+}
+
+НадрукуватиУсіЗмінні(усіЗмінні);
+
+void ОбєднатиЗмінні(string назваМодуля, Dictionary<string, ЗміннаОточення> змінні)
+{
+    foreach (var назва in змінні.Keys)
+    {
+        var змінна = змінні[назва];
+
+        if (усіЗмінні.TryGetValue(назва, out var глобальнаЗмінна))
+        {
+            глобальнаЗмінна.ЄЗмінноюСервісаОточення = змінна.ЄЗмінноюСервісаОточення || глобальнаЗмінна.ЄЗмінноюСервісаОточення;
+            глобальнаЗмінна.ЄЗмінноюОточення = змінна.ЄЗмінноюОточення || глобальнаЗмінна.ЄЗмінноюОточення;
+            глобальнаЗмінна.Модулі.Add(назваМодуля);
+        }
+        else
+        {
+            усіЗмінні.Add(назва, змінна);
+            змінна.Модулі.Add(назваМодуля);
+        }
+    }
+}
+
+static void НадрукуватиЗмінні(string назваМодуля, Dictionary<string, ЗміннаОточення> змінні)
+{
+    Console.WriteLine($"# Модуль {назваМодуля}");
     if (змінні.Count == 0)
     {
         Console.WriteLine();
@@ -67,8 +97,33 @@ foreach (var папкаМодуля in Directory.EnumerateDirectories(папка
     Console.WriteLine();
 }
 
+void НадрукуватиУсіЗмінні(Dictionary<string, ЗміннаОточення> змінні)
+{
+    Console.WriteLine($"# Усі змінні");
+    if (змінні.Count == 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Змінні оточення не використовуються");
+    }
+    else
+    {
+        Console.WriteLine("| Змінна | Оточення | Сервіс | Модулі |");
+        Console.WriteLine("| ------ | -------- | ------ | ---- |");
+        foreach (var назва in змінні.Keys.OrderBy(_ => _))
+        {
+            var змінна = змінні[назва];
+            var модулі = string.Join(", <br>", змінна.Модулі.OrderBy(_ => _));
+            Console.WriteLine($"| {назва,-40} | {(змінна.ЄЗмінноюОточення ? ":white_check_mark:" : ""),-20} | {(змінна.ЄЗмінноюСервісаОточення ? ":white_check_mark:" : ""),-20} | {модулі} |");
+        }
+    }
+
+    Console.WriteLine();
+}
+
 class ЗміннаОточення
 {
     public bool ЄЗмінноюОточення { get; set; }
     public bool ЄЗмінноюСервісаОточення { get; set; }
+
+    public HashSet<string> Модулі { get; set; } = new();
 }
