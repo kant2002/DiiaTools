17de8a1df884a286b0b98bb880c7280b47e2ba52
Andrii Kurdiumov <kant2002@gmail.com>
2024-04-01 20:24:49 +0500

    Переклад протоколювання на українську

--- FakeBankId/Program.cs
+++ FakeBankId/Program.cs
@@ -1,16 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpLogging(options => { });
-//builder.Services.ConfigureHttpJsonOptions(options =>
+var будівник = ВебАплікація.СтворитиБудівника(args);
+будівник.Сервіси.ДодатиHttpПротоколювання(options => { });
+//будівник.Сервіси.ConfigureHttpJsonOptions(options =>
 //{
 //    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 //});
 
-var app = builder.Build();
+var апка = будівник.Побудувати();
 
-app.MapGet("/v1/bank/oauth2/authorize", ([FromQuery(Name = "response_type")]string response_type, [FromQuery(Name = "client_id")] string client_id, [FromQuery(Name = "state")] string state, [FromQuery(Name = "bank_id")] string? bank_id, [FromQuery(Name = "token")] string? token) =>
+апка.MapGet("/v1/bank/oauth2/authorize", ([FromQuery(Name = "response_type")]string response_type, [FromQuery(Name = "client_id")] string client_id, [FromQuery(Name = "state")] string state, [FromQuery(Name = "bank_id")] string? bank_id, [FromQuery(Name = "token")] string? token) =>
 {
     if (token is null)
         return Results.Content("<h1>Hello world</h1>", "text/html");
@@ -19,9 +19,9 @@ app.MapGet("/v1/bank/oauth2/authorize", ([FromQuery(Name = "response_type")]stri
 	return Results.Content($"Redirecting to http://localhost:8080/api/v1/auth/bank-id/code/callback?code={code}.", statusCode: 302);
 });
 
-app.UseHttpsRedirection();
-app.UseHttpLogging();
-app.Run();
+апка.ВикористовуватиПеренаправленняHttps();
+апка.ВикористовуватиHttpПротоколювання();
+апка.Запустити();
 
 //public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
