bf17df1315da36fbf98d2bbe0e8a581075203a58
Andrii Kurdiumov <kant2002@gmail.com>
2024-04-06 18:43:04 +0500

    Додав список методів для повертання інформації о банках

--- FakeBankId/Програма.cs
+++ FakeBankId/Програма.cs
@@ -12,6 +12,15 @@ var апка = будівник.Побудувати();
 var серверДії = "http://localhost:8080";
 var код = "123";
 
+апка.MapGet("/api/banks", () =>
+{
+	return new[]
+	{ 
+		new ІнформаціяБанка("privatbank", "PrivatBank", true, "1436057001", "assets/images/banks/privatbank.png", "https://sandbox.binbu.privatbank.ua/BiNbu/oauth/authorize?response_type=code&client_id=478b231b-c93b-44b1-835e-7e288427ded6&state=", 5),
+	};
+});
+
+
 апка.MapGet("/v1/bank/oauth2/authorize", ([FromQuery(Name = "response_type")] string типЗапиту, [FromQuery(Name = "client_id")] string ідКлієнта, [FromQuery(Name = "state")] string стан, [FromQuery(Name = "bank_id")] string? ідБанка, [FromQuery(Name = "token")] string? token) =>
 {
     if (token is null)
@@ -36,9 +45,11 @@ var код = "123";
 
 public record ІнформаціяКлієнта(string state, string cert, string customerCrypto, string memberId);
 public record ІнформаціяЖетона(string token_type, string access_token, int expires_in);
+public record ІнформаціяБанка(string id, string name, bool workable, string memberId, string logoUrl, string loginUrl, int order);
 
 [JsonSerializable(typeof(ІнформаціяКлієнта))]
 [JsonSerializable(typeof(ІнформаціяЖетона))]
+[JsonSerializable(typeof(ІнформаціяБанка[]))]
 internal partial class СеріалізаційнийКонтекстJsonАпки : JsonSerializerContext
 {
 
